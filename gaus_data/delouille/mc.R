e35d096040521752774df17afd1e501d2fe5fac4
zrxing <zrxing@uchicago.edu>
2016-01-08 19:31:19 -0600

    added motorcycle data

--- gaus_data/delouille/mc.R
+++ gaus_data/delouille/mc.R
@@ -1,36 +1,41 @@
-source(file.path("D:/Grad School/Spring 2013/multiscale_ash/ash/bayesmooth.R"))
-source(file.path("D:/Grad School/Spring 2013/multiscale_ash/ash/bayesmooth_alt2.R"))
-
+library(smash)
 library(MASS)
 
-x.ini=sort(mcycle$times)
-y.ini=mcycle$accel[order(mcycle$times)]
-
-x=unique(x.ini)
-y=0
-for(i in 1:length(x)){
-  y[i]=median(y.ini[x.ini==x[i]])
+#wrapper function to return estimated mean and variance function given raw data
+smash.wrapper = function(x.ini, y.ini){
+  #Take the median of observations with repeated x values
+  x = unique(x.ini)
+  y = 0
+  for(i in 1:length(x)){
+    y[i] = median(y.ini[x.ini == x[i]])
+  }
+  
+  #Mirror the data twice to make it periodic and a power of 2
+  y.exp = c(y, y[length(y):(2*length(y) - 2^ceiling(log2(length(y))) + 1)])
+  y.final = c(y.exp, y.exp[length(y.exp):1])
+  
+  #Run SMASH
+  y.est = ashsmooth.gaus(y.final, v.est=TRUE, joint = TRUE, family = "DaubLeAsymm", filter.number = 8)
+  
+  y.mu.est = y.est$mu.res[1:length(y)]
+  y.var.est = y.est$var.res[1:length(y)]
+  return(list(x = x, y = y, mu.est = y.mu.est, var.est = y.var.est))
 }
 
 
-y.exp=c(y,y[length(y):(2*length(y)-128+1)])
-y.final=c(y.exp,y.exp[length(y.exp):1])
 
-y.est=bayesmooth(y.final)
-y.est.alt2=bayesmooth.alt2(y.final)
+#Obtain initial data
+x.ini = sort(mcycle$times)
+y.ini = mcycle$accel[order(mcycle$times)]
 
-y.est=y.est[1:length(y)]
-y.est.alt2=y.est.alt2[1:length(y)]
+x.ini = sort(ethanol$E)
+y.ini = ethanol$NOx[order(ethanol$E)]
 
-y.var.est=bayesmooth(y.final,v.est=TRUE)
-y.var.est.alt2=bayesmooth.alt2(y.final,v.est=TRUE)
 
-y.var.est=y.var.est[1:length(y)]
-y.var.est.alt2=y.var.est.alt2[1:length(y)]
-
-plot(x,y,ylim=c(-150,100))
-lines(x,y.est,col=2)
-plot(x,y.var.est,type='l')
+plot(x, y, ylim = c(-150,100))
+lines(x, y.mu.est, col = 2)
+lines(x, y.mu.est + 2*sqrt(y.var.est), col = 4)
+lines(x, y.mu.est - 2*sqrt(y.var.est), col = 4)
 
 
 
