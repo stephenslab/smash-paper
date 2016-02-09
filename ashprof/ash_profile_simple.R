68c2f3fd589d643b13b0db3c95cd91d4784c76d5
zrxing <zrxing@uchicago.edu>
2016-02-08 22:06:32 -0600

    updated section on gaussian eg

--- ashprof/ash_profile_simple.R
+++ ashprof/ash_profile_simple.R
@@ -1,37 +1,37 @@
 library(smash)
 
-time_em = matrix(0, nr = 100, nc = 2)
-time_squarem = matrix(0, nr = 100, nc = 2)
-time_ip = matrix(0, nr = 100, nc = 2)
+time_em = matrix(0, nr = 10, nc = 2)
+time_squarem = matrix(0, nr = 10, nc = 2)
+time_ip = matrix(0, nr = 10, nc = 2)
 
 sd = 2
 
 for(i in 1:10){
   b = rnorm(100000, sd = sd)
-  time_em = system.time(ash(b, 1, optmethod = "mixEM"))[1:2]
-  time_squarem = system.time(ash(b, 1, optmethod = "cxxMixSquarem"))[1:2]
-  time_ip = system.time(ash(b, 1, optmethod = "mixIP"))[1:2]
+  time_em[i, ] = system.time(ash(b, 1, optmethod = "mixEM"))[1:2]
+  time_squarem[i, ] = system.time(ash(b, 1, optmethod = "cxxMixSquarem"))[1:2]
+  time_ip[i, ] = system.time(ash(b, 1, optmethod = "mixIP"))[1:2]
 }
 
-summary(time_em)
-summary(time_squarem)
-summary(time_ip)
+summary(time_em[, 1])
+summary(time_squarem[, 1])
+summary(time_ip[, 1])
 
 
 
-time_em = matrix(0, nr = 100, nc = 2)
-time_squarem = matrix(0, nr = 100, nc = 2)
-time_ip = matrix(0, nr = 100, nc = 2)
+time_em = matrix(0, nr = 10, nc = 2)
+time_squarem = matrix(0, nr = 10, nc = 2)
+time_ip = matrix(0, nr = 10, nc = 2)
 
 sd = 0.01
 
 for(i in 1:10){
   b = rnorm(100000, sd = sd)
-  time_em = system.time(ash(b, 1, optmethod = "mixEM"))[1:2]
-  time_squarem = system.time(ash(b, 1, optmethod = "cxxMixSquarem"))[1:2]
-  time_ip = system.time(ash(b, 1, optmethod = "mixIP"))[1:2]
+  time_em[i, ] = system.time(ash(b, 1, optmethod = "mixEM"))[1:2]
+  time_squarem[i, ] = system.time(ash(b, 1, optmethod = "cxxMixSquarem"))[1:2]
+  time_ip[i, ] = system.time(ash(b, 1, optmethod = "mixIP"))[1:2]
 }
 
-summary(time_em)
-summary(time_squarem)
-summary(time_ip)
+summary(time_em[, 1])
+summary(time_squarem[, 1])
+summary(time_ip[, 1])
