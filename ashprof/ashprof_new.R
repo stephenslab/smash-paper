82df4b38bc3ea0213cd6b573ff037506e4b7b7e3
zrxing <zrxing@uchicago.edu>
2016-02-15 12:19:17 -0600

    re-organization of repo

--- ashprof/ashprof_new.R
+++
@@ -1,8 +0,0 @@
-systime = system.time(ashsmooth.gaus(X.s,filter.number=1,family="DaubExPhase"))
-
-tempfile = "ashprofile_ip.txt"
-Rprof(tempfile, interval = 0.1)
-mu.est.haar<-ashsmooth.gaus(X.s,filter.number=1,family="DaubExPhase")
-Rprof(NULL)
-
-summaryRprof(tempfile)
--- ashprof/ashprof_old.R
+++
@@ -1,8 +0,0 @@
-systime = system.time(ashsmooth.gaus(X.s,filter.number=1,family="DaubExPhase"))
-
-tempfile = "ashprofile.txt"
-Rprof(tempfile, interval = 0.1)
-mu.est.haar<-ashsmooth.gaus(X.s,filter.number=1,family="DaubExPhase")
-Rprof(NULL)
-
-summaryRprof(tempfile)
