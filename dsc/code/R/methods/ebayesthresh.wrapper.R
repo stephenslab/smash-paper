5bace12705107c473f826b1ff70a300a5ad09da6
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-07-03 13:06:51 -0500

    Fixed up ebayesthresh.wrapper.R code.

--- dsc/code/R/methods/ebayesthresh.wrapper.R
+++ dsc/code/R/methods/ebayesthresh.wrapper.R
@@ -1,19 +1,24 @@
-# runs wavelet denoising using the Bayesian shrinkage/thresholding procedure EbayesThresh, assuming constant variance
-# inputs: 
-# input: a list containing x: the data, sig.true: the true sigma values, and sig.est: the estimated sigma values
-# args: a list containing family and filter.number, which determine the wavelet basis used 
+# Runs wavelet denoising using the Bayesian shrinkage/thresholding
+# procedure EbayesThresh, assuming constant variance. The inputs are:
 #
-# returns the estimated (posterior mean) mean function
-ebayesthresh.wrapper = function(input, args) {
+#   input, a list containing the data (x), the ground-truth sigma
+#   values (sig.true), and the estimated sigma values (sig.est).
+#
+#   args, a list containing family and filter.number, which determine
+#   the wavelet basis used.
+#
+# The return value is the estimated (posterior) mean function.
+ebayesthresh.wrapper <- function (input, args) {
   if (is.null(args$filter.number)) 
-    args$filter.number = 8
+    args$filter.number <- 8
   if (is.null(args$family)) 
-    args$family = "DaubLeAsymm"
+    args$family <- "DaubLeAsymm"
   
-  n = length(input$x)
-  J = log2(n)
-  x.w <- wd(input$x, args$filter.number, args$family, type = "station")
-  x.w <- ebayesthresh.wavelet(x.w, vscale = input$sig.est, prior = "laplace", threshrule = "mean")
-  mu.est = AvBasis(convert(x.w))
+  n   <- length(input$x)
+  J   <- log2(n)
+  x.w <- wd(input$x,args$filter.number,args$family,type = "station")
+  x.w <- ebayesthresh.wavelet(x.w,vscale = input$sig.est,prior = "laplace",
+                              threshrule = "mean")
+  mu.est <- AvBasis(convert(x.w))
   return(mu.est)
 } 
