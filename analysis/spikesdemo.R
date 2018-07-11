8e426b8d7e3015c2bf67ca617b221c26df9d7419
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-07-11 15:39:47 -0500

    Revised most of the plots for the spikesdemo example.

--- analysis/spikesdemo.R
+++ analysis/spikesdemo.R
@@ -4,10 +4,12 @@ source("../code/plotting.R")
 
 # SET UP ENVIROMENT
 # -----------------
-# Load the ashr and smashr package.
+# Load the ashr, smashr, scales, ggplot2 and cowplot packages.
 library(ashr)
 library(smashr)
-# library(scales)
+library(scales)
+library(ggplot2)
+suppressMessages(library(cowplot))
 
 # Initialize the sequence of pseudorandom numbers.
 set.seed(70915)
@@ -39,16 +41,14 @@ rsnr   <- sqrt(3)
 sig.cb <- sig.cb/mean(sig.cb) * sd(mu.sp)/rsnr^2
 x.sim  <- rnorm(n,mu.sp,sig.cb)
 
-# Plot the simulated data.
-par(cex.axis = 1.8,cex.sub = 1.8,cex.lab = 1.8,mar = c(5.1,5.1,2.1,2.1),
-    mgp = c(3,1.3,0))
-plot(mu.sp,type = 'l',ylim = c(-0.05,0.85),xlab = "",ylab = "",lwd = 1.7)
+# Plot the simulated data set.
+par(cex.axis = 0.8,cex.sub = 1,cex.lab = 1)
+plot(mu.sp,type = 'l',ylim = c(-0.05,1),xlab = "position",
+     ylab = "",lwd = 1.7,xaxp = c(0,1024,4),yaxp = c(0,1,4))
 lines(mu.sp + 2*sig.cb,col = "darkorange",lty = 5,lwd = 1.8)
 lines(mu.sp - 2*sig.cb,col = "darkorange",lty = 5,lwd = 1.8)
 points(x.sim,cex = 0.7,pch = 16,col = "darkblue")
 invisible(readline(prompt = "Press [enter] to continue analysis... "))
-
-stop()
 
 # RUN SMASH
 # ---------
@@ -83,62 +83,50 @@ for(j in 0:(log2(n) - 1)){
 
 # SUMMARIZE RESULTS
 # -----------------
-rbPal <- colorRampPalette(c('#191970','#4169E1','#87CEEB'))
+# Plot the distribution of observed wavelet coefficients.
+par(cex.axis = 0.8,cex.lab = 0.8)
+hist(wc.sim[4,],breaks = 2,xlab = "observed wavelet coefficients",
+     xlim = c(-25,25),ylim = c(0,600),col = "darkblue",xaxp = c(-25,25,10),
+     yaxp = c(0,600,6),main = "")
+hist(wc.sim[10,],breaks = 40,add = TRUE,col = "darkorange")
+invisible(readline(prompt = "Press [enter] to continue analysis... "))
 
-# TO DO: Get rid of this plot.
-## col.3    <- rev(rbPal(100))[as.numeric(cut(wc.pres[[3]],breaks = 100))]
-## wc.sig.3 <- 1/wc.pres[[3]]
-## col.bw.3 <- wc.sig.3*0.8/(max(wc.sig.3) - min(wc.sig.3)) -
-##             (0.8/(max(wc.sig.3) - min(wc.sig.3)))*min(wc.sig.3)
-## plot(wc.sim[4,],wc.sim.shrunk[[3]]$PosteriorMean,xlim = c(-1,1),pch = 20,
-##      ylim = c(-1,1),col = grey(col.bw.3))
+# Plot the observed wavelet coefficients (at scales 1 and 7 only)
+# vs. the "shrunken" wavelet coefficients estimated by adaptive
+# shrinkage.
+par(cex.axis = 0.8,cex.lab = 0.8)
+plot(c(),c(),xlab = "observed wavelet coefficients",
+     ylab = "shrunken wavelet coefficients",
+     xlim = c(-2.5,2.5),ylim = c(-2.5,2.5))
+abline(0,1,lty = 1,col = "gray",lwd = 1)
+points(wc.sim[10,],wc.sim.shrunk[[9]]$PosteriorMean,pch = 20,cex = 0.6,
+       col = "darkorange")
+points(wc.sim[4,],wc.sim.shrunk[[3]]$PosteriorMean,pch = 20,cex = 0.6,
+       col = "darkblue")
+invisible(readline(prompt = "Press [enter] to continue analysis... "))
 
-# TO DO: Get rid of this plot as well.
-## col.9    <- rev(rbPal(100))[as.numeric(cut(wc.pres[[9]],breaks = 100))]
-## wc.sig.9 <- 1/wc.pres[[9]]
-## col.bw.9 <- wc.sig.9*0.7/(max(wc.sig.9) - min(wc.sig.9)) -
-##     (0.7/(max(wc.sig.9) - min(wc.sig.9)))*min(wc.sig.9)
-## plot(wc.sim[10,],wc.sim.shrunk[[9]]$PosteriorMean,xlim = c(-14,14),
-##      pch = 20,ylim = c(-14,14),col = grey(col.bw.9))
+# Plot the observed wavelet coefficients (at scale 7 only) vs. the
+# "shrunken" wavelet coefficients estimated by adaptive shrinkage, and
+# how how the amount of shrinkage depends on the standard error (s.e.)
+# in the observations.
+wc.sig.3 <- 1/wc.pres[[3]]
+p <- ggplot(data.frame(observed = wc.sim[4,],
+                       shrunken = wc.sim.shrunk[[3]]$PosteriorMean,
+                       se       = wc.sig.3),
+            aes(x = observed,y = shrunken,col = se)) +
+  geom_point() +
+  xlim(c(-1,1)) +
+  ylim(c(-1,1)) +
+  scale_color_gradientn(colors = c("deepskyblue","darkblue")) +
+  theme_cowplot()
+print(p)
+invisible(readline(prompt = "Press [enter] to continue analysis... "))
 
-## breaks     <- seq(min(col.bw.3),max(col.bw.3),length.out = 49)
-## breaks.ori <- seq(min(wc.sig.3),max(wc.sig.3),length.out = 50)
-
-pdf("paper/simple_eg_2.pdf",height = 6,width = 8)
-par(cex.axis = 1.8,cex.sub = 1.8,cex.lab = 1.8,mar = c(5.1,5.1,2.1,2.1),mgp = c(3,1.3,0))
-hist(wc.sim[4,],breaks = 2,xlab = "observed wavelet coefficients",xlim = c(-25,25),ylim = c(0,1000),col = grey(0.3),main = "")
-hist(wc.sim[10,],breaks = 40,add = TRUE,col = rgb(0,1,0,0.5))
-legend('topright',legend = c("wavelet coefficients,scale 1","wavelet coefficients,scale 7"),cex = 1.5,fill = c(rgb(0,1,0,0.5),grey(0.1)))
-dev.off()
-
-pdf("paper/simple_eg_3.pdf",height = 6,width = 8)
-par(cex.axis = 1.8,cex.sub = 1.8,cex.lab = 1.8,mar = c(5.1,5.1,2.1,2.1),mgp = c(3,1.3,0))
-plot(wc.sim[10,],wc.sim.shrunk[[9]]$PosteriorMean,xlab = "observed wavelet coefficients",ylab = "shrunken wavelet coefficients",xlim = c(-4,4),pch = 20,cex = 0.8,ylim = c(-4,4),col = rgb(0,1,0,0.5))
-points(wc.sim[4,],wc.sim.shrunk[[3]]$PosteriorMean,pch = 20,cex = 0.8,col = grey(0.1))
-abline(0,1,lty = 3,col = grey(0.3))
-legend('bottomright',legend = c("wavelet coefficients,scale 1","wavelet coefficients,scale 7"),cex = 1.5,pch = c(20,20),col = c(rgb(0,1,0,0.5),grey(0.1)))
-dev.off()
-
-pdf("paper/simple_eg_4.pdf",height = 5.8,width = 8)
-layout(matrix(c(1,1,1,3,2,0),nrow = 3),widths = c(8,2),heights = c(2,4,2))
-par(mar = c(7.1,7.1,2.1,1.1),cex.axis = 2.5,cex.sub = 2.5,cex.lab = 2.5,mgp = c(3,1.5,0))
-plot(wc.sim[4,],wc.sim.shrunk[[3]]$PosteriorMean,xlim = c(-1,1),pch = 20,cex = 1.4,xlab = "",ylab = "",ylim = c(-1,1),col = grey(col.bw.3))
-title(xlab = "observed wavelet coefficients",ylab = "shrunken wavelet coefficients",line = 4)
-legend('bottomright',legend = c("wavelet coefficients,scale 7"),cex = 1.5,pch = c(20,20),col = grey(0.1))
-par(mar = c(1,1,0.1,5.1),cex.axis = 2,cex.sub = 3,cex.lab = 2,mgp = c(3,1.3,0))
-image.scale(volcano,col = grey(breaks),breaks = breaks.ori,horiz = FALSE,yaxt = "n")
-axis(4,at = seq(0,0.25,0.05),las = 2)
-par(mar = c(0.1,0.1,0.1 ,0.1))
-plot(1,1,xlim = c(0,1),ylim = c(0,1),type = "n",axes = FALSE)
-legend(x = -0.25,y = 0.17,legend = "standard error",cex = 1.5,bty = "n")
-dev.off()
-
-pdf("paper/simple_eg_5.pdf",height = 6,width = 8)
-par(cex.axis = 1.8,cex.sub = 1.8,cex.lab = 1.8,mar = c(6.1,6.1,2.1,2.1),mgp = c(3,1.3,0))
+# Plot ...
+par(cex.axis = 0.8)
 plot(mu.sp,type = "l",lty = 2,ylim = c(-0.05,0.85),xlab = "",ylab = "")
 title(xlab = "position",ylab = "reconstructed mean",line = 4)
 lines(mu.ti,col = "blue")
 lines(mu.smash,col = "#FF8C00")
-legend(x = 500,y = 0.9,legend = c("SMASH","TI-thresh (RMAD)"),col = c("#FF8C00","blue"),lty = c(1,1),cex = 1.6,pt.cex = 0.5,bty = "n")
-dev.off()
+
 
