24e20332a569f0c5e45f2ba267623e706d219116
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-08-22 14:27:55 -0500

    Created a better plot for Fig. 2 in plot_gaus.R.

--- analysis/plot_gaus.R
+++ analysis/plot_gaus.R
@@ -2,41 +2,92 @@
 #
 # TO DO: Change the name of this file.
 #
-library(sm)
-library(vioplot)
-source("vioplot_col.R")
+# SET UP ENVIROMENT
+# -----------------
+# Load the sm and vioplot packages, and load the custom violin plot
+# function.
+library(ggplot2)
+library(cowplot)
+# library(sm)
+# library(vioplot)
+source("../code/vioplot_col.R")
 
-load("../../output/gaus-dscr.RData")
+# LOAD RESULTS
+# ------------
+# Load the results of the simulation experiments.
+cat("Loading simulation results.\n")
+load("../output/gaus-dscr.RData")
 
-homo.data.smash = res[res$.id == "sp.3.v1" & res$method == "smash.s8" ,]
-homo.data.smash.homo = res[res$.id == "sp.3.v1" &
-    res$method == "smash.homo.s8" , ]
-homo.data.tithresh = res[res$.id == "sp.3.v1" &
-    res$method == "tithresh.homo.s8" , ]
-homo.data.ebayes = res[res$.id == "sp.3.v1" &
-    res$method == "ebayesthresh" , ]
-homo.data.smash.true = res[res$.id == "sp.3.v1" &
-    res$method == "smash.true.s8" , ]
-homo.data = res[res$.id == "sp.3.v1" &
-    (res$method == "smash.s8" |
-     res$method == "ebayesthresh" |
-     res$method == "tithresh.homo.s8"), ]
+homo.data.smash <-
+  res[res$.id    == "sp.3.v1" &
+      res$method == "smash.s8",]
+homo.data.smash.homo <-
+  res[res$.id    == "sp.3.v1" &
+      res$method == "smash.homo.s8",]
+homo.data.tithresh <-
+  res[res$.id == "sp.3.v1" &
+      res$method == "tithresh.homo.s8",]
+homo.data.ebayes <-
+  res[res$.id    == "sp.3.v1" &
+      res$method == "ebayesthresh",]
+homo.data.smash.true <-
+  res[res$.id == "sp.3.v1" &
+  res$method  == "smash.true.s8",]
+homo.data <-
+  res[res$.id == "sp.3.v1" &
+  (res$method == "smash.s8" |
+   res$method == "ebayesthresh" |
+   res$method == "tithresh.homo.s8"),]
 
 # PLOT #1
 # -------
 # This reproduces Fig. 2 of the manuscript (comparison of accuracy of
 # estimated mean curves for data simulated with homoskedastic Gaussian
 # errors.)
-par(cex.axis = 0.75, cex.lab = 1, cex.sub = 1, mgp = c(3, 1.5, 0))
-vioplot.col(homo.data.smash$mise, homo.data.smash.homo$mise,
-            homo.data.tithresh$mise, homo.data.ebayes$mise,
-            homo.data.smash.true$mise, horizontal = TRUE, ylim = c(5, 18),
-            col = c("magenta", "gold", "gold", "gold", "purple"))
-axis(2, 1:5, labels = c("SMASH", "SMASH\nhomo",
-                 "TI-thresholding\nhomo", "EbayesThresh\nhomo",
-                 "SMASH\ntrue variance"), las = 2)
+pdat <-
+  rbind(data.frame(method      = "smash",
+                   method.type = "est",
+                   mise        = homo.data.smash$mise),
+        data.frame(method      = "smash.homo",
+                   method.type = "homo",
+                   mise        = homo.data.smash.homo$mise),
+        data.frame(method      = "tithresh",
+                   method.type = "homo",
+                   mise        = homo.data.tithresh$mise),
+        data.frame(method      = "ebayesthresh",
+                   method.type = "homo",
+                   mise        = homo.data.ebayes$mise),
+        data.frame(method      = "smash.true",
+                   method.type = "true",
+                   mise        = homo.data.smash.true$mise))
+pdat <-
+  transform(pdat,
+            method = factor(method,
+                            names(sort(tapply(pdat$mise,pdat$method,mean),
+                                       decreasing = TRUE))))
+p <- ggplot(pdat,aes(x = method,y = mise,fill = method.type)) +
+     geom_violin(fill = "skyblue",color = "skyblue") +
+     geom_boxplot(width = 0.15,outlier.shape = NA) +
+     scale_y_continuous(breaks = seq(6,16,2)) +
+     scale_fill_manual(values = c("darkorange","dodgerblue","gold"),
+                       guide = FALSE) +
+     coord_flip() +
+     labs(x = "MISE",y = "") +
+     theme(axis.line = element_blank(),
+           axis.ticks.y = element_blank())
+print(p)
+        par(cex.axis = 0.75,cex.lab = 1,cex.sub = 1)
+vioplot.col(homo.data.smash$mise,
+            homo.data.smash.homo$mise,
+            homo.data.tithresh$mise,
+            homo.data.ebayes$mise,
+            homo.data.smash.true$mise,horizontal = TRUE,ylim = c(5,18),
+            col = c("magenta","gold","gold","gold","purple"))
+axis(2,1:5,las = 2,
+     labels = c("SMASH","SMASH (homo)","TI-thresholding (homo)",
+                "EbayesThresh (homo)","SMASH (true variance)"))
 title(xlab = "MISE", line = 3.5)
-abline(v = median(homo.data.smash$mise), lty = 3, col = 3)
+abline(v = median(homo.data.smash$mise),lty = 3,col = 3)
 abline(h = 4.5, lty = 3)
 
 stop()
@@ -45,15 +96,13 @@ cblocks.fn = function(t, type) {
   pos = c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
   hgt = 2.88/5 * c(4, (-5), 3, (-4), 5, (-4.2), 2.1, 4.3, (-3.1), 2.1, (-4.2))
   fn = rep(0, length(t))
-  for (j in 1:length(pos)) {
+  for (j in 1:length(pos))
     fn = fn + (1 + sign(t - pos[j])) * (hgt[j]/2)
-  }
   fn[fn < 0] = 0
-  if (type == "mean") {
+  if (type == "mean")
     return(NULL)
-  } else if (type == "var") {
+  else if (type == "var")
     return(0.01 + 1 * (fn - min(fn))/max(fn))
-  }
 }
 
 doppler.fn = function(t, type) {
@@ -82,9 +131,10 @@ cor.fn = function(t, type) {
 }
 
 spikes.fn = function(t, type) {
-  spike.f = function(x) (0.75 * exp(-500 * (x - 0.23)^2) +
-      1.5 * exp(-2000 * (x - 0.33)^2) +
-      3 * exp(-8000 * (x - 0.47)^2) +
+  spike.f = function(x)
+    (0.75  * exp(-500 * (x - 0.23)^2) +
+      1.5  * exp(-2000 * (x - 0.33)^2) +
+      3    * exp(-8000 * (x - 0.47)^2) +
       2.25 * exp(-16000 * (x - 0.69)^2) +
       0.5 * exp(-32000 * (x - 0.83)^2))
   fn = spike.f(t)
