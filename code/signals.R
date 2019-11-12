3729045bef9e80f0ed5ca4ab26ab6e163f89566a
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-11-12 16:31:47 -0600

    Fixed up R code in signals.R.

--- code/signals.R
+++ code/signals.R
@@ -1,129 +1,145 @@
 # This file defines some of the mean, variance and intensity functions
 # used to simulate Gaussian and Poisson data sets.
 
-spike.f <- function(x)
-  (0.75 * exp(-500   * (x - 0.23)^2) +
-   1.5  * exp(-2000  * (x - 0.33)^2) +
-   3    * exp(-8000  * (x - 0.47)^2) + 
-   2.25 * exp(-16000 * (x - 0.69)^2) +
-   0.5  * exp(-32000 * (x - 0.83)^2))
+# This defines the "Spikes" mean function.
+spike.fn <- function (t, type = c("mean","var")) {
+  type <- match.arg(type)
+  f    <- spike.f(t)
+  if (type == "mean")
+    return((1 + f)/5)
+  else if (type == "var")
+    return(NULL)
+}
 
+# This is used by spikes.fn to define the "Spikes" mean and variance
+# functions.
+spike.f <- function (x)
+  0.75 * exp(-500   * (x - 0.23)^2) +
+  1.5  * exp(-2000  * (x - 0.33)^2) +
+  3    * exp(-8000  * (x - 0.47)^2) + 
+  2.25 * exp(-16000 * (x - 0.69)^2) +
+  0.5  * exp(-32000 * (x - 0.83)^2)
+
+# This defines the "Bumps" mean and variance functions.
+bumps.fn <- function (t, type = c("mean","var")) {
+  type <- match.arg(type)
+  pos  <- c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
+  hgt  <- 2.97/5*c(4, 5, 3, 4, 5, 4.2, 2.1, 4.3, 3.1, 5.1, 4.2)
+  wth  <- c(0.005, 0.005, 0.006, 0.01, 0.01, 0.03, 0.01, 0.01, 0.005,
+            0.008, 0.005)
+  f = rep(0,length(t))
+  for (i in 1:length(pos))
+    f <- f + hgt[i]/((1 + (abs(t - pos[i])/wth[i]))^4)
+  if (type == "mean")
+    return((1 + f)/5)
+  else if (type == "var")
+    return(1e-05 + f)
+}
+
+# This defines the "Blocks" mean and variance functions.
+blocks.fn <- function (t, type = c("mean","var")) {
+  type <- match.arg(type)
+  pos  <- c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
+  hgt  <- 2.88/5 * c(4, -5, 3, -4, 5, -4.2, 2.1, 4.3, -3.1, 2.1, -4.2)
+  f    <- rep(0,length(t))
+  for (j in 1:length(pos))
+    f <- f + (1 + sign(t - pos[j])) * hgt[j]/2
+  if (type == "mean")
+    return(0.2 + 0.6 * (f - min(f))/max(f - min(f)))
+  else if (type == "var")
+    return(NULL)
+}
+
+# This defines the "Angles" mean function.
+angles.fn <- function (t, type = c("mean","var")) {
+  s <- ((2 * t + 0.5) * (t <= 0.15)) +
+       ((-12 * (t - 0.15) + 0.8) * (t > 0.15 & t <= 0.2)) +
+       0.2 * (t > 0.2 & t <= 0.5) +
+       ((6 * (t - 0.5) + 0.2) * (t > 0.5 & t <= 0.6)) +
+       ((-10 * (t - 0.6) + 0.8) * (t > 0.6 & t <= 0.65)) +
+       ((-0.5 * (t - 0.65) + 0.3) * (t > 0.65 & t <= 0.85)) +
+       ((2 * (t - 0.85) + 0.2) * (t > 0.85))
+  f <- 3/5 * ((5/(max(s) - min(s))) * s - 1.6) - 0.0419569
+  if (type == "mean")
+    return((1 + f)/5)
+  else if (type == "var")
+    return(NULL)
+}
+
+# This defines the "Doppler" mean and variance functions.
+doppler.fn <- function (t, type = c("mean","var")) {
+  type <- match.arg(type)
+  f    <- dop.f(t)
+  if (type == "mean") {
+    f <- 3/(max(f) - min(f)) * (f - min(f))
+    return((1 + f)/5)
+  } else if (type == "var") {
+    f <- 10 * f
+    f <- f - min(f)
+    return(1e-05 + 2*f)
+  }
+}
+
+# This is used by doppler.fn to define the "Doppler" mean and variance
+# functions.
 dop.f <- function(x)
   sqrt(x * (1 - x)) * sin((2 * pi * 1.05)/(x + 0.05))
 
-cblocks.fn <- function(t, type) {
-  pos = c(0.1,0.13,0.15,0.23,0.25,0.4,0.44,0.65,0.76,0.78,0.81)
-  hgt = 2.88/5 * c(4,-5,3,-4,5,-4.2,2.1,4.3,-3.1,2.1,-4.2)
-  fn  = rep(0,length(t))
+# This defines the "Blip" mean function.
+blip.fn <- function (t, type = c("mean","var")) {
+  f <- (0.32 + 0.6 * t + 0.3 * exp(-100 * (t - 0.3)^2)) * (t >= 0 & t <= 0.8) +
+       (-0.28 + 0.6 * t + 0.3 * exp(-100 * (t - 1.3)^2)) * (t > 0.8 & t <= 1)
+  if (type == "mean")
+    return(f)
+  else if (type == "var")
+    return(NULL)
+}
+
+# This defines the "Corner" mean function.
+cor.fn <- function(t, type = c("mean","var")) {
+  type <- match.arg(type)
+  f    <- 623.87 * t^3 * (1 - 2 * t) * (t >= 0 & t <= 0.5) +
+          187.161 * (0.125 - t^3) * t^4 * (t > 0.5 & t <= 0.8) +
+          3708.470441 * (t - 1)^3 * (t > 0.8 & t <= 1)
+  f    <- (0.6/(max(f) - min(f))) * f
+  if (type == "mean")
+    return(f - min(f) + 0.2)
+  else if (type == "var")
+    return(NULL)
+}
+
+# This defines the constant variance function.
+cons.fn = function (t, type = c("var","mean")) {
+  f <- rep(1,length(t))
+  if (type == "mean") {
+    return(NULL)
+  } else if (type == "var") {
+    return(f)
+  }
+}
+  
+# This defines the "Clipped Blocks" variance function.
+cblocks.fn <- function (t, type = c("var","mean")) {
+  pos <- c(0.1,0.13,0.15,0.23,0.25,0.4,0.44,0.65,0.76,0.78,0.81)
+  hgt <- 2.88/5 * c(4,-5,3,-4,5,-4.2,2.1,4.3,-3.1,2.1,-4.2)
+  f   <- rep(0,length(t))
   for (j in 1:length(pos))
-    fn = fn + (1 + sign(t - pos[j])) * (hgt[j]/2)
-  fn[fn < 0] = 0
+    f <- f + (1 + sign(t - pos[j])) * (hgt[j]/2)
+  f[f < 0] <- 0
   if (type == "mean")
     return(NULL)
   else if (type == "var")
-    return(0.01 + 1 * (fn - min(fn))/max(fn))
+    return(0.01 + 1 * (f - min(f))/max(f))
 }
 
-doppler.fn <- function(t, type) {
-  dop.f = function(x) sqrt(x * (1 - x)) * sin((2 * pi * 1.05)/(x + 0.05))
-  fn    = dop.f(t)
-  if (type == "mean") {
-    fn = 3/(max(fn) - min(fn)) * (fn - min(fn))
-    return((1 + fn)/5)
-  } else if (type == "var") {
-    fn = 10 * fn
-    fn = fn - min(fn)
-    return(1e-05 + 2 * fn)
-  }
-}
-
-cor.fn <- function(t, type) {
-  fn = 623.87 * t^3 * (1 - 2 * t) * (t >= 0 & t <= 0.5) +
-       187.161 * (0.125 - t^3) * t^4 * (t > 0.5 & t <= 0.8) +
-       3708.470441 * (t - 1)^3 * (t > 0.8 & t <= 1)
-  fn = (0.6/(max(fn) - min(fn))) * fn
+  # This defines the "triple exponential" variance function.
+texp.fn <- function(t, type = c("var","mean")) {
+  f <- 1e-04 + 4*(exp(-550 * (t - 0.2)^2) +
+                  exp(-200 * (t - 0.5)^2) +
+                  exp(-950 * (t - 0.8)^2))
   if (type == "mean")
-    return(fn - min(fn) + 0.2)
+    return(NULL)
   else if (type == "var")
-    return(NULL)
-}
-
-spikes.fn <- function(t, type) {
-  spike.f = function(x)
-    (0.75 * exp(-500   * (x - 0.23)^2) +
-     1.5  * exp(-2000  * (x - 0.33)^2) +
-     3    * exp(-8000  * (x - 0.47)^2) +
-     2.25 * exp(-16000 * (x - 0.69)^2) +
-     0.5  * exp(-32000 * (x - 0.83)^2))
-  fn = spike.f(t)
-  if (type == "mean")
-    return((1 + fn)/5)
-  else if (type == "var")
-    return(NULL)
-}
-
-cons.fn = function(t, type) {
-  fn = rep(1, length(t))
-  if (type == "mean") {
-    return(NULL)
-  } else if (type == "var") {
-    return(fn)
-  }
-}
-
-bumps.fn = function(t, type) {
-  pos = c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
-  hgt = 2.97/5 * c(4, 5, 3, 4, 5, 4.2, 2.1, 4.3, 3.1, 5.1, 4.2)
-  wth = c(0.005, 0.005, 0.006, 0.01, 0.01, 0.03, 0.01, 0.01, 0.005, 0.008, 0.005)
-  fn = rep(0, length(t))
-  for (j in 1:length(pos)) {
-    fn = fn + hgt[j]/((1 + (abs(t - pos[j])/wth[j]))^4)
-  }
-  if (type == "mean") {
-    return((1 + fn)/5)
-  } else if (type == "var") {
-    return(1e-05 + fn)
-  }
-}
-
-blocks.fn = function(t, type) {
-  pos = c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
-  hgt = 2.88/5 * c(4, (-5), 3, (-4), 5, (-4.2), 2.1, 4.3, (-3.1), 2.1, (-4.2))
-  fn = rep(0, length(t))
-  for (j in 1:length(pos)) {
-    fn = fn + (1 + sign(t - pos[j])) * (hgt[j]/2)
-  }
-  if (type == "mean") {
-    return(0.2 + 0.6 * (fn - min(fn))/max(fn - min(fn)))
-  } else if (type == "var") {
-    return(NULL)
-  }
-}
-
-angles.fn = function(t, type) {
-  sig = ((2 * t + 0.5) * (t <= 0.15)) + ((-12 * (t - 0.15) + 0.8) * (t > 0.15 & t <= 0.2)) + 0.2 * (t > 0.2 & t <= 0.5) + ((6 * (t - 0.5) + 0.2) * (t > 0.5 & t <= 0.6)) + ((-10 * (t - 0.6) + 0.8) * (t > 0.6 & t <= 0.65)) + ((-0.5 * (t - 0.65) + 0.3) * (t > 0.65 & t <= 0.85)) + ((2 * (t - 0.85) + 0.2) * (t > 0.85))
-  fn = 3/5 * ((5/(max(sig) - min(sig))) * sig - 1.6) - 0.0419569
-  if (type == "mean") {
-    return((1 + fn)/5)
-  } else if (type == "var") {
-    return(NULL)
-  }
-}
-
-blip.fn = function(t, type) {
-  fn = (0.32 + 0.6 * t + 0.3 * exp(-100 * (t - 0.3)^2)) * (t >= 0 & t <= 0.8) + (-0.28 + 0.6 * t + 0.3 * exp(-100 * (t - 1.3)^2)) * (t > 0.8 & t <= 1)
-  if (type == "mean") {
-    return(fn)
-  } else if (type == "var") {
-    return(NULL)
-  }
-}
-
-texp.fn = function(t, type) {
-  fn = 1e-04 + 4 * (exp(-550 * (t - 0.2)^2) + exp(-200 * (t - 0.5)^2) + exp(-950 * (t - 0.8)^2))
-  if (type == "mean") {
-    return(NULL)
-  } else if (type == "var") {
-    return(fn)
-  }
+    return(f)
+  
 }
