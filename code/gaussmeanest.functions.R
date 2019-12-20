97bc4e86ca2076c0e6853fb9b23c02492e9571d6
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-12-20 14:11:37 -0600

    Implemented function create.bar.plots in gaussmeanest.functions.R.

--- code/gaussmeanest.functions.R
+++ code/gaussmeanest.functions.R
@@ -26,4 +26,34 @@ get.results.homosked <- function (res, scenario) {
                           mise        = dat.smash.true$mise)))
 }
 
+# This function is used to draw the bar charts summarizing the results
+# of the Gaussian data simulations.
+create.bar.plots <- function (pdat) {
     
+  # Compute the mean, upper and lower quantiles, which will be used to
+  # plot the barchart bars and the error bars.
+  pdat <- with(pdat,tapply(mise,list(sim,snr,method),
+                         function (x) c(mean(x),quantile(x,c(0.1,0.9)))))
+  pdat <- adply(pdat,1:3)
+  names(pdat) <- c("simulation","snr","method","mean","low","high")
+
+  # Re-order the factor levels in the "method" column.
+  pdat <- transform(pdat,
+                    method = factor(method,c("smash.true","smash.homo","smash",
+                                             "tithresh","ebayesthresh")))
+
+  # Create the bar charts.
+  return(ggplot(pdat,aes_string(x = "method",y = "mean",fill = "method")) +
+         geom_col(width = 0.65) +
+         geom_errorbar(aes_string(x = "method",ymin = "low",ymax = "high"),
+                       color = "black",inherit.aes = FALSE,width = 0.3,
+                       size = 0.4) +
+         scale_x_discrete(breaks = NULL) +
+         scale_fill_manual(values = c("gold","darkorange","indianred",
+                                      "darkblue","royalblue")) +
+         facet_grid(snr ~ simulation,scales = "free") +
+         labs(x = "",y = "MISE") +
+         theme_cowplot(font_size = 12) +
+         theme(strip.background = element_blank()))
+}
+         
